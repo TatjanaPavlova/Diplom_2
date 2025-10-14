23a38746c713fd370ff2084823a4c876d6f40310
Tatiana Pavlova <tanya.pavlova3008@gmail.com>
2025-10-14 17:52:03 +0300

    готова структура проекта

---
+++ data.py
@@ -0,0 +1,7 @@
+
+
+class Url:
+    BASE_URL = 'https://stellarburgers.education-services.ru' # главная страница Stellar Burgers
+    USER_CREATE = '/api/auth/register' # создание пользователя
+    USER_LOGIN = '/api/auth/login' # логин пользователя
+    ORDER_CREATE = '/api/orders' # создание заказа
